0f1174d564ea2d5a8a9e6fc20a6049412edfb585
gauss5930 <kopilot100@gmail.com>
2023-10-23 15:16:26 +0900

    17th Commit(Retry!!)

--- SFT/SFTTrainer_lora.py
+++ SFT/SFTTrainer_lora.py
@@ -6,7 +6,7 @@ from peft import AutoPeftModelForCausalLM, LoraConfig
 from tqdm import tqdm
 import random
 
-from trl import SFTTrainer
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from trl.trainer import ConstantLengthDataset
 from accelerate import Accelerator
 
@@ -35,16 +35,16 @@ def args_parse():
     parser.add_argument("--per_device_train_batch_size", type=int, default=2)
     parser.add_argument("--per_device_eval_batch_size", type=int, default=4)
     parser.add_argument("--group_by_length", type=bool, default=False)
-    parser.add_argument("--packing", type=bool, default=True)
+    parser.add_argument("--packing", type=bool, default=False)
 
     parser.add_argument("--lora_alpha", type=int, default=16)
     parser.add_argument("--lora_dropout", type=float, default=0.05)
     parser.add_argument("--lora_r", type=int, default=8)
 
-    parser.add_argument("--learning_rate", type=float, default=1e-5)
-    parser.add_argument("--lr_scheduler_type", type=str, default="cosine")
+    parser.add_argument("--learning_rate", type=float, default=5e-7)
+    parser.add_argument("--lr_scheduler_type", type=str, default="linear")
     parser.add_argument("--warmup_ratio", type=float, default=0.1)
-    parser.add_argument("--weight_decay", type=float, default=0.0)
+    parser.add_argument("--weight_decay", type=float, default=0)
 
     parser.add_argument(
         "--output_dir",
@@ -59,18 +59,6 @@ def args_parse():
 
     return parser.parse_args()
 
-def chars_token_ratio(dataset, tokenizer, nb_examples=500):
-    total_characters, total_tokens = 0, 0
-    for _, example in tqdm(zip(range(nb_examples), iter(dataset)), total=nb_examples):
-        text = process_dataset(example)
-        total_characters += len(text)
-        if tokenizer.is_fast:
-            total_tokens += len(tokenizer(text).tokens())
-        else:
-            total_tokens += len(tokenizer.tokenize(text))
-
-    return total_characters / total_tokens
-
 def dataset_sampling(dataset, sample_size):
     random_indices = random.sample(range(len(dataset)), sample_size)
 
@@ -78,31 +66,34 @@ def dataset_sampling(dataset, sample_size):
 
     return random_samples
 
-def prompt_formatting(dataset):
-    result = ""
-    for data in dataset:
-        if data["role"] == "user":
-            result += f"<|user|>\n{data['content']}</s>"
-        elif data["role"] == "assistant":
-            result += f"<|assitant|>\n{data['content']}</s>"
-        elif data["role"] == "system":
-            result += f"<|system|>\n{data['content']}</s>"
-
-    return result
-
 def process_dataset(example):
-    instruction_prompt = []
-    for i in range(len(example["data"])):
-        if (i + 1) % 2 != 0:
-            instruction_prompt.append({"role": "user", "content": example["data"][i]})
-        else:
-            instruction_prompt.append({"role": "asisstant", "content": example["data"][i]})
 
-    result_data = prompt_formatting(instruction_prompt)
+    def prompt_formatting(dataset):
+        result = ""
+        for data in dataset:
+            if data["role"] == "user":
+                result += f"<|user|>\n{data['content']}</s>"
+            elif data["role"] == "assistant":
+                result += f"<|assitant|>\n{data['content']}</s>"
+            elif data["role"] == "system":
+                result += f"<|system|>\n{data['content']}</s>"
+
+        return result
+    
+    result_data = []
+    for n in range(len(example)):
+        instruction_prompt = []
+        for i in range(len(example["data"][n])):
+            if (i + 1) % 2 != 0:
+                instruction_prompt.append({"role": "user", "content": example["data"][n][i]})
+            else:
+                instruction_prompt.append({"role": "asisstant", "content": example["data"][n][i]})
+
+        result_data.append(prompt_formatting(instruction_prompt))
 
     return result_data
 
-def create_datasets(tokenizer, args):
+def create_datasets(args):
     dataset = load_dataset(
         args.dataset_name,
         split=args.split,
@@ -116,27 +107,7 @@ def create_datasets(tokenizer, args):
     train_data = dataset["train"]
     valid_data = dataset["test"]
 
-    chars_per_token = chars_token_ratio(train_data, tokenizer)
-
-    train_dataset = ConstantLengthDataset(
-        tokenizer,
-        train_data,
-        formatting_func=process_dataset,
-        infinite=True,
-        seq_length=args.seq_length,
-        chars_per_token=chars_per_token,
-    )
-
-    valid_dataset = ConstantLengthDataset(
-        tokenizer,
-        valid_data,
-        formatting_func=process_dataset,
-        infinite=False,
-        seq_length=args.seq_length,
-        chars_per_token=chars_per_token,
-    )
-
-    return train_dataset, valid_dataset
+    return train_data, valid_data
 
 if __name__ == "__main__":
     args = args_parse()
@@ -147,6 +118,7 @@ if __name__ == "__main__":
         trust_remote_code=True,
     )
     model.config.use_cache = False
+    model.enable_input_require_grads()
 
     peft_config = LoraConfig(
         r=args.lora_r,
@@ -190,12 +162,17 @@ if __name__ == "__main__":
         remove_unused_columns=False,
     )
 
-    train_dataset, eval_dataset = create_datasets(tokenizer, args)
+    train_dataset, eval_dataset = create_datasets(args)
+
+    response_template = "<|assistant?|\n"
+    collator = DataCollatorForCompletionOnlyLM(response_template=response_template, tokenizer=tokenizer)
 
     trainer = SFTTrainer(
         model=model,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
+        formatting_func=process_dataset,
+        data_collator=collator,
         peft_config=peft_config,
         packing=args.packing,
         max_seq_length=args.seq_length,
